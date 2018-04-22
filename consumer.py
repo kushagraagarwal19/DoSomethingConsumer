357cea7a32892b88f7d3d73ef1a0721526512bce
kushagraagarwal19 <kushagra.agarwal@outlook.com>
2018-04-22 09:29:11 -0400

    Changes

--- consumer.py
+++ consumer.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+url = 'amqp://spbdyewq:0PfDzWG1GiXAe5pNVv4u6WPdl9qF60cf@eagle.rmq.cloudamqp.com/spbdyewq'
+params = pika.URLParameters(url)
+params.socket_timeout = 5
 
-channel.queue_declare(queue='q')
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='q', durable = 'false')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+  print(" [x] Received %r" % body)
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback, queue='q',no_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
+print(' [*] Waiting for messages:')
 channel.start_consuming()
